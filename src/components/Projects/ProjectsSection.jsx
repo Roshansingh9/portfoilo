5cdb94ee49a4338f958dd304a05bab5f959d31fe
Roshan kr singh <roshan.kr.singh9911@gmail.com>
2025-05-22 21:07:16 +0530

    fix 2.0

--- src/components/Projects/ProjectsSection.jsx
+++ src/components/Projects/ProjectsSection.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { motion } from "framer-motion";
 import ProjectCard from "./ProjectCard";
-import ProjectsData from "./Projects.json";
+import ProjectsData from "./projects.json";
 
 const ProjectsSection = () => {
   // Animation variants for staggered children animation
